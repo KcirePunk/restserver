a91c3c6256470f3a4e04766a9eb6e34106d94bbd
KcirePunk <kcire_792_efta@hotmail.com>
2019-12-21 11:28:26 -0500

    RestServer Terminado

---
+++ server/config/config.js
@@ -0,0 +1,18 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb+srv://kcirepunk:1OJvkVEL9F3iTUpn@cluster0-chnab.mongodb.net/cafe';
+} else {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}
+
+process.env.URLDB = urlDB;
